88c6b94566a78d0d968c4dfab3f7285c04f8e011
Karl-maker <karljohanbailey98@gmail.com>
2025-02-07 11:28:21 -0400

    fix: add find method

--- src/tests/__mocks__/iot.device.repository.ts
+++ src/tests/__mocks__/iot.device.repository.ts
@@ -1,7 +1,12 @@
 import { IotDeviceRepository } from "../../domain/repositories/iot.device.repository";
 import { IotDevice } from "../../domain/entities/iot.device.entity";
+import { IotDeviceSortBy, IotDeviceFilterBy } from "../../domain/types/types/ios.device.type";
+import { FindParams, FindResponse } from "../../domain/types/types/repository.type";
 
 export class MockIotDeviceRepository implements IotDeviceRepository {
+    find(params?: FindParams<IotDeviceSortBy, IotDeviceFilterBy> | undefined): Promise<FindResponse<IotDevice>> {
+        throw new Error("Method not implemented.");
+    }
     // Mock the save method to return a resolved promise with a device that includes an id
     save(device: IotDevice): Promise<IotDevice> {
         return Promise.resolve({
